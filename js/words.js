7b22cc0417d774fc70fb366e35569c80299c3261
XcenqX <53969735+xcenqx@users.noreply.github.com>
2024-10-02 20:09:06 +0200

    Create words.js

---
+++ js/words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "dodawanie",
+        hint: "Proces dodawania liczb"
+    },
+    {
+        word: "spotkanie",
+        hint: "Wydarzenie, podczas którego spotykają się ludzie"
+    },
+    {
+        word: "liczba",
+        hint: "Symbol matematyczny używany do liczenia"
+    },
+    {
+        word: "wymiana",
+        hint: "Akt handlu"
+    },
+    {
+        word: "płótno",
+        hint: "Kawałek materiału do malowania olejnego"
+    },
+    {
+        word: "ogród",
+        hint: "Miejsce do sadzenia kwiatów i roślin"
+    },
+    {
+        word: "pozycja",
+        hint: "Lokalizacja kogoś lub czegoś"
+    },
+    {
+        word: "pióro",
+        hint: "Włosy przypominające zewnętrzną powłokę ptaka"
+    },
+    {
+        word: "komfort",
+        hint: "Przyjemne uczucie relaksu"
+    },
+    {
+        word: "język",
+        hint: "Mięśniowy narząd jamy ustnej"
+    },
+    {
+        word: "ekspansja",
+        hint: "Proces zwiększania lub wzrostu"
+    },
+    {
+        word: "kraj",
+        hint: "Rejon zidentyfikowany politycznie"
+    },
+    {
+        word: "grupa",
+        hint: "Wiele obiektów lub osób"
+    },
+    {
+        word: "smak",
+        hint: "Zdolność języka do wykrywania smaku"
+    },
+    {
+        word: "supermarket",
+        hint: "Duży sklep, w którym handluje się towarami"
+    },
+    {
+        word: "pole",
+        hint: "Powierzchnia gruntu pod działalność rolniczą"
+    },
+    {
+        word: "przyjaciel",
+        hint: "Osoba inna niż członek rodziny"
+    },
+    {
+        word: "kieszeń",
+        hint: "Torba do przenoszenia drobnych przedmiotów"
+    },
+    {
+        word: "igła",
+        hint: "Cienka i ostra metalowa szpilka"
+    },
+    {
+        word: "ekspert",
+        hint: "Osoba z rozległą wiedzą"
+    },
+    {
+        word: "oświadczenie",
+        hint: "deklaracja czegoś"
+    },
+    {
+        word: "drugi",
+        hint: "Jedna sześćdziesiąta minuty"
+    },
+    {
+        word: "biblioteka",
+        hint: "Miejsce zawierające księgozbiór"
+    },
+]
